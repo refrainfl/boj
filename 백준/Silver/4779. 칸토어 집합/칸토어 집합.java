3a3baee60d6a4a97b68ed501244545ce6293059b
refrainfl <refrain2k@gmail.com>
2023-06-19 20:26:06 +0900

    [Silver III] Title: 칸토어 집합, Time: 268 ms, Memory: 32772 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    static String cantor(int depth) {
+        if (depth == 0) return "-";
+        if (depth == 1) return "- -";
+
+        String str = " ";
+        for (int i = 0; i < depth - 1; i++) {
+            str = str.repeat(3);
+        }
+        return cantor(depth - 1) + str + cantor(depth - 1);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = "";
+        while ((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input);
+            sb.append(cantor(N)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
