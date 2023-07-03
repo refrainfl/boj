671c227ee646f0f8cff99f5160973932653441f5
refrainfl <refrain2k@gmail.com>
2023-07-03 10:29:02 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 632 ms, Memory: 31932 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        TreeMap<String, Integer> map = new TreeMap<>((o1, o2) -> {
+                if (o1.length() > o2.length()) return -1;
+                else if (o1.length() < o2.length()) return 1;
+                else return o1.compareTo(o2);
+        });
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if (word.length() < M) continue;
+            if (map.containsKey(word)) map.put(word, map.get(word) + 1);
+            else map.put(word, 0);
+        }
+
+        List<String> result = new ArrayList<>(map.keySet());
+        result.sort((o1, o2) -> -(map.get(o1) - map.get(o2)));
+
+        for(String s :result) sb.append(s).append("\n");
+        System.out.println(sb);
+    }
+}
