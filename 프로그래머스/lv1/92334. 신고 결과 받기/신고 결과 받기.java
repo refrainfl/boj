6861849ce30e50cdf774490a6060b63f1f3f5799
refrainfl <refrain2k@gmail.com>
2023-08-14 18:05:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,37 +3,27 @@ import java.util.*;
 class Solution {
     public static int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-
-        StringTokenizer st;
         HashMap<String, List<String>> reportedMap = new HashMap<>();
 
         for (String id : id_list) {
             reportedMap.put(id, new ArrayList<>());
         }
 
+        StringTokenizer st;
         for (String rp : report) {
             st = new StringTokenizer(rp);
             String reporter = st.nextToken();
             String target = st.nextToken();
-
-            if (reportedMap.get(target).contains(reporter)) continue;
-            reportedMap.get(target).add(reporter);
+            if (!reportedMap.get(target).contains(reporter)) reportedMap.get(target).add(reporter);
         }
-
 
         for (String id : id_list) {
             if (reportedMap.get(id).size() >= k) {
                 for (String toMail : reportedMap.get(id)) {
-                    for (int i = 0; i < id_list.length; i++) {
-                        if (id_list[i].equals(toMail)) {
-                            answer[i]++;
-                            break;
-                        }
-                    }
+                    answer[Arrays.asList(id_list).indexOf(toMail)]++;
                 }
             }
         }
-
 
         return answer;
     }
