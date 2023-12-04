3eb1d9dd13601a9152467bc3f86c3230f29fd11f
refrainfl <refrain2k@gmail.com>
2023-12-04 12:03:23 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 18420 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -2,13 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
 
-    static final int MAXSIZE = 100000;
+    //0-1 BFS
+
+    static final int MAX_SIZE = 100000;
+    static final int NOT_VISITED = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,41 +24,46 @@ class Main {
     }
 
     private static int bfs(int n, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] distance = new int[MAXSIZE + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Deque<Integer> dq = new LinkedList<>();
+        int[] distance = new int[MAX_SIZE + 1];
+        Arrays.fill(distance, NOT_VISITED);
         distance[n] = 0;
-        q.add(n);
+        dq.add(n);
 
-        while (!q.isEmpty()) {
-            int current = q.poll();
+        while (!dq.isEmpty()) {
+            int current = dq.poll();
             int currentDistance = distance[current];
 
-            int left = current - 1;
-            int right = current + 1;
+            if (current == k) {
+                return currentDistance;
+            }
+
             int teleport = current * 2;
-
-            if (isPositionAvailable(left) && currentDistance + 1 < distance[left]) {
-                distance[left] = currentDistance + 1;
-                q.add(left);
-            }
-
-            if (isPositionAvailable(right) && currentDistance + 1 < distance[right]) {
-                distance[right] = currentDistance + 1;
-                q.add(right);
-            }
-
-            if (isPositionAvailable(teleport) && currentDistance < distance[teleport]) {
+            if (isPositionAvailable(teleport) && distance[teleport] == NOT_VISITED) {
                 distance[teleport] = currentDistance;
-                q.add(teleport);
+                dq.addFirst(teleport);
             }
+
+            int left = current - 1;
+            if (isPositionAvailable(left) && distance[left] == NOT_VISITED) {
+                distance[left] = currentDistance + 1;
+                dq.add(left);
+            }
+
+            int right = current + 1;
+            if (isPositionAvailable(right) && distance[right] == NOT_VISITED) {
+                distance[right] = currentDistance + 1;
+                dq.add(right);
+            }
+
         }
 
         return distance[k];
     }
 
     private static boolean isPositionAvailable(int position) {
-        if (position < 0 || MAXSIZE < position) return false;
+        if (position < 0 || MAX_SIZE < position) return false;
         return true;
     }
+
 }
