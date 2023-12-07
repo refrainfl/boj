b99ed6cf5929278da4489298b55cf215689595c1
refrainfl <refrain2k@gmail.com>
2023-12-07 16:07:08 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 532 ms, Memory: 96116 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -4,36 +4,50 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-    static int[] list;
-
-    static int binary_search(int target, int start, int end) {
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (target > list[mid]) start = mid + 1;
-            else end = mid;
-        }
-        return end;
-    }
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        list = new int[10000001];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        list[0] = arr[0];
-        int idx = 0;
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            if (arr[i] > list[idx]) list[++idx] = arr[i];
-            else {
-                if (i == N - 1) break;
-                int header = binary_search(arr[i], 0, idx);
-                list[header] = arr[i];
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(findLIS(arr));
+
+    }
+
+    private static int findLIS(int[] arr) {
+        int maxLen = arr.length;
+        int[] dp = new int[maxLen];
+        dp[0] = arr[0];
+
+        int LIS = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (dp[LIS - 1] < arr[i]) {
+                dp[LIS++] = arr[i];
+            } else {
+                int idx = LowerBound(dp, arr[i], 0, LIS);
+                dp[idx] = arr[i];
+            }
+
+        }
+
+        return LIS;
+    }
+
+    private static int LowerBound(int[] dp, int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (dp[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        System.out.println(idx + 1);
+        return end + 1;
     }
 }
