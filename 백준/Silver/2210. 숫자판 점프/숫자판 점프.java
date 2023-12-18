e75c36b5e2859033728d78703369da8c1be044cf
refrainfl <refrain2k@gmail.com>
2023-12-18 16:05:44 +0900

    [Silver II] Title: 숫자판 점프, Time: 128 ms, Memory: 15156 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -1,43 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 class Main {
 
-    static final int boardSize = 5;
     static final int[] dx = new int[]{0, 1, 0, -1};
     static final int[] dy = new int[]{1, 0, -1, 0};
-    static int[][] board = new int[boardSize][boardSize];
-    static HashSet<String> resultMap = new HashSet<>();
+    static final int boardSize = 5;
+    static final int[][] board = new int[boardSize][boardSize];
+    static boolean[] visited = new boolean[1000000];
+    static int cnt;
 
 
     public static void main(String[] args) throws IOException {
-
         input();
         for (int i = 0; i < boardSize; i++) {
             for (int j = 0; j < boardSize; j++) {
-                makeString(i, j, 0, "");
+                makeString(i, j, 0, 0);
             }
         }
-        System.out.println(resultMap.size());
+        System.out.println(cnt);
 
     }
 
-    private static void makeString(int x, int y, int depth, String result) {
+    private static void makeString(int x, int y, int depth, int val) {
         if (depth == 6) {
-            resultMap.add(result);
+            if (!visited[val]) {
+                visited[val] = true;
+                cnt++;
+            }
             return;
         }
 
-        result += board[x][y];
+        val += (int) (board[x][y] * (Math.pow(10,depth)));
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (0 <= nx && nx < boardSize && 0 <= ny && ny < boardSize) {
-                makeString(nx, ny, depth + 1, result);
+                makeString(nx, ny, depth + 1, val);
             }
         }
     }
