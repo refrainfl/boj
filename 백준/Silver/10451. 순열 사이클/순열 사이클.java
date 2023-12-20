05044e3bef58cae6409c49b0603c1db06316e9d3
refrainfl <refrain2k@gmail.com>
2023-12-20 19:02:08 +0900

    [Silver III] Title: 순열 사이클, Time: 536 ms, Memory: 51208 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -2,12 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.IntStream;
 
 class Main {
 
     static int[] arr;
-    static int[] visited;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,29 +17,32 @@ class Main {
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
             arr = new int[N];
-            visited = new int[N];
+            visited = new boolean[N];
 
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken()) - 1;
             }
 
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
-                if (visited[i] == 0) dfs(i, i);
+                if (!visited[i]) {
+                    dfs(i);
+                    cnt++;
+                }
             }
 
-            int[] result = IntStream.of(visited).distinct().toArray();
-            sb.append(result.length).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         System.out.println(sb);
 
     }
 
-    private static void dfs(int start, int cur) {
-        if (visited[cur] == start) return;
-        visited[cur] = start;
-        dfs(start, arr[cur]);
+    private static void dfs(int cur) {
+        if (visited[cur]) return;
+        visited[cur] = true;
+        dfs(arr[cur]);
     }
 
 }
