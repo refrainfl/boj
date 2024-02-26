e00416795ae3c6d267c8f377ba2487b33aac49fb
refrainfl <refrain2k@gmail.com>
2024-02-26 13:31:40 +0900

    [Gold V] Title: 적록색약, Time: 164 ms, Memory: 17964 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,63 +3,53 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
 
     static int N;
     static char[][] board;
-
-    static boolean[][] visitedNormal;
-    static boolean[][] visitedBlindness;
-
-    static int normalSection;
-    static int blindnessSection;
+    static boolean[][][] visited;
+    static int[] result = new int[2];
+    static int[][] dxdy = {{0, 0, -1, 1}, {-1, 1, 0, 0}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         board = new char[N][N];
-        visitedNormal = new boolean[N][N];
-        visitedBlindness = new boolean[N][N];
+        visited = new boolean[2][N][N];
 
         for (int i = 0; i < N; i++) {
             board[i] = br.readLine().toCharArray();
         }
 
         for (int i = 0; i < N; i++) {
-
             for (int j = 0; j < N; j++) {
-
-                if (!visitedNormal[i][j]) {
-                    normalSection++;
+                if (!visited[0][i][j]) {
+                    result[0]++;
                     dfs(i, j, false);
                 }
 
-                if (!visitedBlindness[i][j]) {
-                    blindnessSection++;
+                if (!visited[1][i][j]) {
+                    result[1]++;
                     dfs(i, j, true);
                 }
 
             }
         }
 
-        System.out.println(normalSection + " " + blindnessSection);
+        System.out.println(result[0] + " " + result[1]);
 
     }
 
     private static void dfs(int y, int x, boolean hasBlindness) {
         if (!hasBlindness) {
-            visitedNormal[y][x] = true;
+            visited[0][y][x] = true;
         } else {
-            visitedBlindness[y][x] = true;
+            visited[1][y][x] = true;
         }
 
         char currentColor = board[y][x];
         for (int i = 0; i < 4; i++) {
-            
-            int ry = y + dy[i];
-            int rx = x + dx[i];
+            int ry = y + dxdy[0][i];
+            int rx = x + dxdy[1][i];
 
             if (rangeCheck(ry, rx)
                     && sameCheck(currentColor, board[ry][rx], hasBlindness)
@@ -71,7 +61,7 @@ class Main {
     }
 
     private static boolean visitedCheck(int y, int x, boolean hasBlindness) {
-        return hasBlindness ? visitedBlindness[y][x] : visitedNormal[y][x];
+        return hasBlindness ? visited[1][y][x] : visited[0][y][x];
     }
 
     private static boolean sameCheck(char currenColor, char nextColor, boolean hasBlindness) {
